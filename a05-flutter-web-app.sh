032bda523900fbc849ac927f815468750887c1d4
Jeremy Ellis <keyfreemusic@gmail.com>
2019-11-14 11:05:49 -0800

    Create a05-flutter-web-app.sh

---
+++ a05-flutter-web-app.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#export ANDROID_SDK_ROOT="/workspace/rocksetta-gitpod-links/android/"
+#echo $ANDROID_SDK_ROOT
+
+#export PATH="${PATH}:${ANDROID_HOME}tools/:${ANDROID_HOME}platform-tools/"
+#export PATH="${PATH}:$/workspace/rocksetta-gitpod-links/cordova/node_modules/:/workspace/rocksetta-gitpod-links/cordova/node_modules/cordova/bin:/workspace/rocksetta-gitpod-links/cordova/node_modules/qrcode/bin"
+
+#echo $PATH
+
+
+#start the web server
+apachectl start
+
+echo "Enter a folder name, no spaces, no dashes, underscore allowed, for your app and folder example: helloWorld"
+read wow4
+
+flutter create $wow4 com.$wow4.$wow4 $wow4
+#cordova create hello com.example.hello HelloWorld
+
+
+cd $wow4
+
+
+#cordova platform add android
+
+
+#cordova build android --no-telemetry
+
+flutter run -d web-server --web-port=8080 --web-hostname=0.0.0.0
+
+#cp /workspace/my-gitpod-flutter/$wow4/platforms/android/app/build/outputs/apk/debug/app-debug.apk /workspace/my-gitpod-flutter/public/$wow4-app-debug.apk
+
+
+
+#qrcode -o /workspace/my-gitpod-flutter/public/qrout.png $(gp url 8001);
+#echo "<br><img src='qrout.png'><br>" >> /workspace/my-gitpod-flutter/public/index.html
+
+#printf "\n\n<a href='$wow4/platforms/android/app/build/outputs/apk/$wow4-debug.apk'>$wow4/platforms/android/app/build/outputs/apk/$wow4-debug.apk
+
+#echo "<li><a href="$wow4-app-debug.apk">$wow4-app-debug.apk</a><br>" >> /workspace/my-gitpod-flutter/public/index.html
+
+
+#open the preview pane
+gp preview $(gp url 8080)
+
+#echo "Refresh your web browser!"
