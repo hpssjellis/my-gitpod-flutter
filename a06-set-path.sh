e73c73b0fcf3351e5b78056317551e6a60306c23
Jeremy Ellis <keyfreemusic@gmail.com>
2019-12-16 23:32:21 -0800

    Create a06-set-path.sh

---
+++ a06-set-path.sh
@@ -0,0 +1,11 @@
+
+
+      #deletes the new java
+      export PATH=${PATH/':/home/gitpod/.sdkman/candidates/java/current/bin'/}   
+
+      #sets old java 8 only for present window
+      export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/   
+      export PATH=$PATH:$JAVA_HOME        
+      echo "$JAVA_HOME"
+      java --version
+      javac --version
